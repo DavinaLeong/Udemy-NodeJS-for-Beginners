aa58a515e0bbeb3f60561d92f0fbe09b701a88ab
Davina LSY <leong.shi.yun@gmail.com>
2018-03-27 11:22:32 +0800

    Messing around with module.exports.

--- node/helpers.js
+++ node/helpers.js
@@ -3,9 +3,7 @@ console.log('line 1', module);
 module.exports.title = 'NODE';
 console.log('line 4', module.exports.title);
 
-module.exports.sum = (num1, num2) => {
-    console.log(`${num1} + ${num2} = ` + (num1 + num2));
-};
-console.log('line 9', module.exports.sum(1, 2));
+const LIB = require('./lib');
+console.log('line 9', LIB.sum(546, 434));
 
 console.log('line 11', module.exports);
---
+++ node/lib.js
@@ -0,0 +1,7 @@
+module.exports = {
+    sum: (num1, num2) => {
+        let sum = num1 + num2;
+        console.log(`sum: ${num1}+${num2}=` + sum);
+        return sum;
+    }
+};
